5a77eeb16e22fad2bd60f0a9110b28455cc9fddb
Kirill <kirillgoodline@gmail.com>
2024-08-16 14:53:37 +0300

    add word-counter package

---
+++ word-counter-package/word-counter.go
@@ -0,0 +1,39 @@
+package word_counter
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"sort"
+)
+
+func WordCounter() {
+	scan := bufio.NewScanner((os.Stdin))
+	words := make(map[string]int)
+
+	scan.Split(bufio.ScanWords)
+
+	for scan.Scan() {
+		words[scan.Text()]++
+	}
+
+	fmt.Println(len(words), "unique words")
+
+	type kv struct {
+		key string
+		val int
+	}
+
+	var ss []kv
+	for k, v := range words {
+		ss = append(ss, kv{k, v})
+	}
+
+	sort.Slice(ss, func(i, j int) bool {
+		return ss[i].val > ss[j].val
+	})
+
+	for _, s := range ss[:3] {
+		fmt.Println(s.key, "appears", s.val, "times")
+	}
+}
